885d1c416e655fc4d8362adb971342ae71a4c60d
Kohei Ozeki <K.odeveloper10@gmail.com>
2018-02-19 22:34:01 +0900

    ch09

---
+++ src/ch09/ex01/bank/bank1.go
@@ -0,0 +1,47 @@
+package bank
+
+var deposits = make(chan int)
+var balances = make(chan int)
+var withdraw = make(chan *WithdrawType)
+
+func Deposit(amount int) {
+	deposits <- amount
+}
+
+func Balance() int {
+	return <-balances
+}
+
+func teller() {
+	var balance int
+	for {
+		select {
+		case amount := <-deposits:
+			balance += amount
+		case balances <- balance:
+		case wd := <-withdraw:
+			if balance >= wd.amount {
+				balance -= wd.amount
+				wd.ch <- true
+			} else {
+				wd.ch <- false
+			}
+
+		}
+	}
+}
+
+type WithdrawType struct {
+	amount int
+	ch     chan bool
+}
+
+func Withdraw(amount int) bool {
+	ch := make(chan bool)
+	withdraw <- &WithdrawType{amount, ch}
+	return <-ch
+}
+
+func init() {
+	go teller()
+}
